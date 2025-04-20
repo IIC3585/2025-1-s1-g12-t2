5bca5f4cd8042ac3adce9161d707706f9bd1d20b
Kevin Cespedes Arancibia <kev.cespedes.arancibia@gmail.com>
2025-04-20 12:36:25 -0400

    Add image showing after upload AND Improve upload style

--- app/src/views/MainPage.js
+++ app/src/views/MainPage.js
@@ -1,8 +1,9 @@
 import Box from '@mui/material/Box';
 import { styled, Button, Typography } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import ImageIcon from '@mui/icons-material/Image';
 import init, { resize, grayscale, blur } from '../pkg/img.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -15,6 +16,11 @@ const VisuallyHiddenInput = styled('input')({
     whiteSpace: 'nowrap',
     width: 1,
 });
+
+const buttonSx = {
+    margin: 1,
+    width: '200px',
+};
 
 function downloadImage(imageData, fileName) {
     const blob = new Blob([imageData], { type: 'image/png' });
@@ -38,18 +44,69 @@ function applyToImage(fn, args){
     }
 }
 
+function MediaBox({imageURL, setImageURL}){
+    const hoverColor = '#5863cc';
+    const mediaBoxSx = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: 500,
+        border: '2px dashed',
+        borderColor: '#ccc',
+        borderRadius: 4,
+        backgroundColor: '#f5f5f5',
+        color: '#cccccc',
+        '&:hover': {
+            borderColor: hoverColor,
+            cursor: 'pointer',
+        },
+        '&:hover .child': {
+            color: hoverColor, // Change child styles on hover
+        }
+    };
+    const imageBoxSx = {
+        objectFit: "contain",
+        maxHeight: '100%',
+        maxWidth: '100%',
+    }
+    let image = <Box component="img"
+        sx={imageBoxSx}
+        alt="The house from the offer."
+        src={imageURL}
+    />
+    let initialMessage = <>
+        <ImageIcon className='child' fontSize='inherit' style={{ fontSize: '10rem' }}/>
+        <Typography className='child' variant="h6" component="h2" gutterBottom>
+            Click to upload an image
+        </Typography>
+    </>
+    return (<Box sx={mediaBoxSx} onClick={() => document.getElementById('imageInput').click()}>
+        {imageURL ? image : initialMessage}
+        <VisuallyHiddenInput
+                id="imageInput"
+                type="file"
+                onChange={(event) => {
+                    const file = event.target.files[0];
+                    if (!file) return;
+                    const url = URL.createObjectURL(file);
+                    console.log(url);
+                    setImageURL(url);
+                    }}
+                multiple
+            />
+    </Box>)
+}
+
 function MainPage(){
     let sx = {
         padding: 5,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+        height: '100%',
     }
-    const buttonSx = {
-        margin: 1,
-        width: '200px',
-    };
-
     let deferredPrompt;
     useEffect(() => {
         const handleBeforeInstallPrompt = (e) => {
@@ -61,7 +118,7 @@ function MainPage(){
             window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
         };
     }, []);
-
+    const [imageURL, setImageURL] = useState(null);
     const handleInstallClick = async () => {
         if (deferredPrompt) {
             deferredPrompt.prompt();
@@ -74,52 +131,54 @@ function MainPage(){
             deferredPrompt = null;
         }
     };
-
+    
+    let filtersBoxSx = {
+        display: 'flex',
+        flexDirection: { xs:'column', sm: 'column', md: 'row'},
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        border: '2px dashed #ccc',
+        borderRadius: 4,
+    }
     return (
         <Box sx={sx}>
             <Typography variant="h4" component="h1" gutterBottom>
                 Image Processing
             </Typography>
-            <Button
-                sx={buttonSx}
-                component="label"
-                role={undefined}
-                variant="contained"
-                tabIndex={-1}
-                startIcon={<CloudUploadIcon />}
-            >
-                Upload Image
-                <VisuallyHiddenInput
-                    id="imageInput"
-                    type="file"
-                    onChange={(event) => console.log(event.target.files)}
-                    multiple
-                />
-            </Button>
-            <Button 
-                id="resizeButton" 
-                variant="contained"
-                sx={buttonSx}
-                onClick={applyToImage(resize, [200, 200])}
-            >
-                Resize
-            </Button>
-            <Button 
-                id="grayScaleButton" 
-                variant="contained"
-                sx={buttonSx}
-                onClick={applyToImage(grayscale, [])}
-            >
-                Gray Scale
-            </Button>
-            <Button 
-                id="blurButton" 
-                variant="contained"
-                sx={buttonSx}
-                onClick={applyToImage(blur, [5.0])}
-            >
-                Blur
-            </Button>
+            <MediaBox imageURL={imageURL} setImageURL={setImageURL}/>
+            <Box sx={'display: flex; flex-direction: column; justify-content: center;'}>
+                <Typography variant="h6" component="h2">
+                        Filters
+                </Typography>
+                <Box sx={filtersBoxSx}>
+                    <Button 
+                        id="resizeButton" 
+                        variant="contained"
+                        sx={buttonSx}
+                        onClick={applyToImage(resize, [200, 200])}
+                    >
+                        Resize
+                    </Button>
+                    <Button 
+                        id="grayScaleButton" 
+                        variant="contained"
+                        sx={buttonSx}
+                        onClick={applyToImage(grayscale, [])}
+                    >
+                        Gray Scale
+                    </Button>
+                    <Button 
+                        id="blurButton" 
+                        variant="contained"
+                        sx={buttonSx}
+                        onClick={applyToImage(blur, [5.0])}
+                    >
+                        Blur
+                    </Button>
+                </Box>
+            </Box>
+            
             <Button 
                 id="installButton" 
                 variant="contained"
